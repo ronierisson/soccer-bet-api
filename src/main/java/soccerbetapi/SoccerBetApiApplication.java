2b52fd1bddb32f8cd18937936b6ab82833a9dd68
ronierisson <ronierisson.souza@gmail.com>
2020-02-28 21:59:57 -0300

    Story-1 [Roniérisson] Create project

---
+++ src/main/java/soccerbetapi/SoccerBetApiApplication.java
@@ -0,0 +1,13 @@
+package soccerbetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerBetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerBetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soccerbetapi/SoccerBetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package soccerbetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerBetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
