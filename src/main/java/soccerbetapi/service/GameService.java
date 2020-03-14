8439b7d485154c61f759d0e44a771bf20983cf4f
Roniérisson <ronierisson.souza@gmail.com>
2020-03-14 12:06:01 -0300

    Codacy [Roniérisson] Fixing Codacy code style

--- src/main/java/soccerbetapi/service/GameService.java
+++ src/main/java/soccerbetapi/service/GameService.java
@@ -1,16 +1,11 @@
 package soccerbetapi.service;
 
-import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.criterion.Projections;
-import org.hibernate.tool.schema.Action;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import soccerbetapi.repository.GameRepository;
 import soccerbetapi.response.GameResponse;
-
-import java.util.Objects;
 
 @Service
 @Slf4j
